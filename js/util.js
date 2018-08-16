e66e5c3d78437c1d58aae4bad6cce39840027a80
Michael Budnick <michaeldbudnick@gmail.com>
2018-08-16 13:18:33 -0400

    Create simple table

---
+++ js/util.js
@@ -0,0 +1,25 @@
+if (typeof Object.keys !== "function") {
+    (function() {
+        var hasOwn = Object.prototype.hasOwnProperty;
+        console.log('adding keys')
+        Object.keys = Object_keys;
+        function Object_keys(obj) {
+            var keys = [], name;
+            for (name in obj) {
+                if (hasOwn.call(obj, name)) {
+                    keys.push(name);
+                }
+            }
+            return keys;
+        }
+    })();
+}
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
